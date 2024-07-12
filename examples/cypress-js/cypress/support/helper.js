288f3d6347bbfc25b7fa84d181fb609f51516d79
Taisia Pitko <taisia.pitko@greenlight.guru>
2024-07-12 10:24:37 +0200

    js as well

--- examples/cypress-js/cypress/support/helper.js
+++ examples/cypress-js/cypress/support/helper.js
@@ -1,4 +1,4 @@
-export const htmlFormatted = `
+export const htmlFormatted = (htmlBody, script) =>  `
     <html>
     <head>
     <style>
@@ -34,6 +34,7 @@ export const htmlFormatted = `
 </head>
     
     <body>
+        ${htmlBody}
         <div class="dialog-container">
             <div class="dialog">
               <div class="title">Dialog title</div>
@@ -63,12 +64,13 @@ export const htmlFormatted = `
         </div>
     </body>
     </html>
+    <script>${script}</script>
     `;
 
 
 export const visitHtml = (options) => {
   cy.intercept('mytest.com**', {
-    body: options?.htmlOverride ?? htmlFormatted,
+    body: options?.htmlOverride ?? htmlFormatted(options?.additionalBodyHtml, options?.script),
     delayMs: options?.delayBeforeLoad ?? 200,
   });
   cy.visit('mytest.com', options?.visitTimeout ? { timeout: options.visitTimeout } : {});
